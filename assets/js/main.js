0a064ace546c080ba709388e083acce165985321
ImBenja <162513288+ImBenja@users.noreply.com>
2024-06-06 19:49:28 -0300

    actualizo archivos

---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function openSearch() {
+  document.getElementById("searchBox").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("searchBox").style.display = "none";
+}
+
+function OponHelp() {
+  document.getElementById("MyBenja").style.display = "block";
+}
+function CloseBenja() {
+  document.getElementById("MyBenja").style.display = "none";
+}
